165160ef2559612cbce121ae559268e3d3ea90bf
ifurkanveli <ifurkanveli@gmail.com>
2022-12-20 11:09:16 +0300

    C05_LocalDate

---
+++ src/BenimAlistirmalar/Static03.java
@@ -0,0 +1,23 @@
+package BenimAlistirmalar;
+
+public class Static03 {
+
+
+        static String ad = "Ali Can";
+
+        static int kilo = 33;
+
+        public static void main(String[] args) {
+
+        }
+
+        public static void artirma() {
+
+            kilo++;
+
+            System.out.println(kilo);
+
+        }
+
+    }
+
