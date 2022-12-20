d687e82bf423a13f306c31fa6fc91ba8737ae211
ifurkanveli <ifurkanveli@gmail.com>
2022-12-20 13:40:44 +0300

    C06_Varargs

--- src/day23_dateTime_varargs/C06_Varargs.java
+++ src/day23_dateTime_varargs/C06_Varargs.java
@@ -3,5 +3,33 @@ package day23_dateTime_varargs;
 public class C06_Varargs {
     public static void main(String[] args) {
 
+        // oyle bir method olusturayım ki
+        // kac tane argument yazarsam yazayım
+        // argumentlerin toplamını yazsın
+
+        topla(5,6); // 11
+        topla(3,4,5,6,7,1,2,3); // 31
+
+        /*
+        Varargs array alt yapısını kullanır
+        Temel hedef argument sayısı değişse bile
+        aynı data turundeki tum argumentleri alabilecek bir parametre olusturmaktır.
+         */
+    }
+
+    private static void topla(int... sayılar) {
+        int toplam1=0;
+        for (int i = 0; i < sayılar.length ; i++) {
+            toplam1 += sayılar[i];
+        }
+        System.out.println(toplam1);
+
+        int toplam2=0;
+        for (Integer each:sayılar
+             ) {
+            toplam2+=each;
+
+        }
+        System.out.println(toplam2);
     }
 }
