a5d2e43f75bb141fa6183e970b42efcb139d822c
ifurkanveli <ifurkanveli@gmail.com>
2022-12-21 20:11:38 +0300

    C22_örnekler

---
+++ src/sameQuestions/C22_örnekler.java
@@ -0,0 +1,9 @@
+package sameQuestions;
+
+public class C22_örnekler {
+    public static void main(String[] args) {
+        System.out.println("Furkan");
+        System.out.println("Veli");
+        System.out.println("inal");
+    }
+}
