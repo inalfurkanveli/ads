545caf5de253e29a99172a550282fae6ff0af871
Furkan Veli İnal <ifurkanveli@gmail.com>
2022-12-27 11:49:50 +0300

    C02_StringBuilder

--- src/day24_StringBuilder/C02_StringBuilder.java
+++ src/day24_StringBuilder/C02_StringBuilder.java
@@ -31,5 +31,40 @@ public class C02_StringBuilder {
 
 
 
+        StringBuilder sb2= new StringBuilder(sb.toString().toUpperCase());
+        System.out.println(sb2); // JAVA
+
+        sb=sb2;
+        System.out.println(sb);
+
+        sb.setCharAt(0,'H');
+        System.out.println(sb); // HAVA
+
+        System.out.println(sb.substring(2, 3)); // V
+
+        System.out.println(sb.reverse()); // AVAH
+
+        System.out.println(sb.insert(1, "H")); // AYVAH
+
+        sb.insert(5," ETMEK YOK CALISMAK VAR",0,10);
+
+        System.out.println(sb);
+
+        System.out.println(sb.indexOf("A", 3)); // 3
+
+        System.out.println(sb.indexOf("VAH")); // 2
+
+        System.out.println(sb.lastIndexOf("VAH")); //2
+
+        System.out.println(sb.lastIndexOf("E", 9)); // 9
+
+        sb.replace(0,15,"Sorun Yok");
+        System.out.println(sb); // Sorun Yok
+
+        sb.delete(5,sb.length()); // Baslangic index'i dahil bitis index'i dahil değil
+        System.out.println(sb);
+
+
+
     }
 }
