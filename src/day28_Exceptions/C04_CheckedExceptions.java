ff5dca570cb3485d39b27ac05cb682a4f8c8230d
Furkan Veli İnal <ifurkanveli@gmail.com>
2023-01-03 13:29:07 +0300

    C04_CheckedExceptions

--- src/day28_Exceptions/C04_CheckedExceptions.java
+++ src/day28_Exceptions/C04_CheckedExceptions.java
@@ -8,5 +8,22 @@ public class C04_CheckedExceptions {
 
 
         FileInputStream fis= new FileInputStream("src/day28_Exceptions/metin.txt");
+        /*
+        Bazi kodları yazdıgımızda java Compile time sırasında
+        o kodlarda exceptions riskini görür ve bizden bir çözüm bekler
+
+
+        Ozellikle dosya okuma ve yazma ile ilgili
+        IO ( İnput/output) islemlerinde ya okuyamazsam ya yazamazsam diyerek
+        bizden compile time'da çözüm ister
+
+        Bu durumda biz exception ile karşılaşıldıgında
+        kod dursun diyorsak : method satırına throws keyword kullanarak
+                             beklenilen exception(lar)i deklare edebiliriz.
+                            Bu sadece olayın farkında oldugumuzu deklare eder
+                            Exception firlatmanin ve kodu durdurmanin onune gecmez
+
+       Kod calismaya devam etsin : try-catch-(finally) blogu kullanabiliriz.
+         */
     }
 }
