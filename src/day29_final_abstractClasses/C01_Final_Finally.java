a2c8ee0d74eb20253929109ae8a0508588d47230
Furkan Veli İnal <ifurkanveli@gmail.com>
2023-01-04 13:11:51 +0300

    C01_Final_Finally

---
+++ src/day29_final_abstractClasses/C01_Final_Finally.java
@@ -0,0 +1,18 @@
+package day29_final_abstractClasses;
+
+public class C01_Final_Finally {
+
+    final int sayi=10;
+    final static String okulIsmı="Yildiz Koleji";
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE); // 2147483647
+        System.out.println(Math.PI); // 3.141592653589793
+
+        // final olarak tanımlanan veriable'lara sonradan deger atanamaz
+        // bir convention olarak final veriable isimleri buyuk harfle yazılır.
+
+
+
+    }
+}
